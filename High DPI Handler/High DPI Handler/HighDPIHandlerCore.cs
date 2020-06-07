e0663a70f6c5c4025e1f82c6fd687e7b82629d42
lockieluke <sherlockluk@outlook.com>
2020-06-07 11:42:14 +0800

    Initial Release
    First Release!!!

---
+++ High DPI Handler/High DPI Handler/HighDPIHandlerCore.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net.Mime;
+
+namespace High_DPI_Handler
+{
+    public class HighDPIHandler
+    {
+        public static void EnableHighDPISupport()
+        {
+            if (Environment.OSVersion.Version.Major >= 6)
+                SetProcessDPIAware();
+        }
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool SetProcessDPIAware();
+    }
+}
